a6d6f0f8d733808e9bd998768ce8799d75abe92c
Greyson Jennings <tallesttower1@gmail.com>
2020-03-31 22:22:27 +0200

    fixed navigation. a bit wonky, but b god, it works.

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -1,16 +1,17 @@
 extends RigidBody2D
 
 onready var player = get_parent().get_node("player")
+onready var target = get_position()
 export var state = "IDLE"
 export var last_seen = Vector2()
 export var suspicion_level = 0
 const CURIOUS_TIMER_MAX = 5
 const SEARCHING_TIMER = 15
+const SNAP_DIST = 40
 const SPEED = 40
 var angle = 0
 var seenTimer = 0
 var searchTimer = SEARCHING_TIMER
-var target = Vector2()
 var go = Vector2()
 var path
 
@@ -48,6 +49,7 @@ func _process(delta):
 			else: #otherwise
 				seenTimer = 0 #reset timer
 				state = "IDLE" #lost player 
+				target = get_position()
 				suspicion_level += 1 #raise alert level
 		if state == "FOUND": #if found player but not in sight
 			state = "CURIOUS" #curious
@@ -56,8 +58,8 @@ func _process(delta):
 	print(state)
 	#MOVEMENT LOOP
 	path = get_parent().get_node("Nav").get_simple_path(get_position(),target)
-	for i in path:
-		go = get_position().direction_to(i).normalized()
-	#TODO: Replace this with ASTAR
+	if get_position().distance_to(path[1]) > SNAP_DIST:
+		go = get_position().direction_to(path[1]).normalized()
+
 	apply_central_impulse(go*SPEED)
 	
