98ce0ed3638f7ac009ba8d4709a7bbd4e9ed680b
Greyson Jennings <tallesttower1@gmail.com>
2020-04-12 15:33:41 +0200

    Added "huh?" state, with flashlight swinging

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -8,14 +8,16 @@ export var suspicion_level = 0
 export var FOV = 90
 export var alert = 0
 export var flashlightSwingCurve: Curve
-const CURIOUS_TIMER_MAX = 5
+const CURIOUS_TIMER_MAX = 7
 const SEARCHING_TIMER = 15
+const HUH_MAX = 5
 const SNAP_DIST = 10
 const SPEED = 40
 var sightDistance = 1000
 var angle = 0
 var seenTimer = 0
 var searchTimer = SEARCHING_TIMER
+var huhTimer = 0
 var go = Vector2()
 var flashlightInterp = 0
 var flashlightSpeed = .25
@@ -31,30 +33,54 @@ func _process(delta):
 	match state:
 		"IDLE":
 			#Point in the driection that the enemy is moving
+			#TODO: add patrol path
 			angle = rad2deg(Vector2().angle_to_point(go))
 		"CURIOUS","FOUND":
 			#If it is at the target, swing flashlight around in search of the player
 			#If not, point towards target
 			if is_at_target():
+				#This controls the swinging animation by interpolating across a curve object
 				flashlightInterp += delta*flashlightSpeed
 				if flashlightInterp > 1:
 					flashlightInterp = 0
 				angle += flashlightSwingCurve.interpolate(flashlightInterp) * flashlightAmplitude
 			else:
 				angle = rad2deg(get_position().angle_to_point(target))
+		"HUH":
+			flashlightInterp += delta*flashlightSpeed
+			if flashlightInterp > 1:
+				flashlightInterp = 0
+			angle += flashlightSwingCurve.interpolate(flashlightInterp) * flashlightAmplitude
+			if huhTimer <= HUH_MAX:
+				huhTimer += delta
+			else:
+				huhTimer = 0
+				state = "IDLE"
 	#behavior that *does* involve player detection
 	if canSeePlayer():
 		var ppos = player.get_position()
 		var distance = get_position().distance_to(ppos)
+		print(str(distance/sightDistance) + " " + state + " " + str(alert))
 		match state:
 			"IDLE":
 				#If the guard sees the player, go into curious mode
 				#If the player is right in front of the guard, then the player is found immediately
-				
-				#TODO: add "huh?" past 2/3
-				if distance/sightDistance > (1.0/3.0):
+				if distance/sightDistance > (2.0/3.0):
+					state = "HUH"
+					angle = rad2deg(get_position().angle_to_point(ppos))
+					huhTimer = 0
+				elif distance/sightDistance <= (2.0/3.0) and distance/sightDistance >= (1.0/3.0):
 					state = "CURIOUS"
-				else:
+					searchTimer = 0
+				elif distance/sightDistance < (1.0/3.0):
+					state = "FOUND"
+			"HUH":
+				#HUH means the guard is looking at where they Think they saw the player
+				print(distance/sightDistance)
+				if distance/sightDistance <= (2.0/3.0) and distance/sightDistance >= (1.0/3.0):
+					state = "CURIOUS"
+					searchTimer = 0
+				elif distance/sightDistance < (1.0/3.0):
 					state = "FOUND"
 			"CURIOUS":
 				#If sees player, move toward player and count up timer
@@ -105,7 +131,6 @@ func _process(delta):
 	
 	$Flashlight.swing_to_direction(deg2rad(angle))#set_rotation(deg2rad(angle))
 	#print(angle)
-	print(state)
 	apply_central_impulse(go*SPEED)
 
 func canSeePlayer():
