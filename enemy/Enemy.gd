cf6ed0a68ed48ceef422f243b60b9f5dca94be80
Greyson Jennings <tallesttower1@gmail.com>
2020-04-12 13:58:46 +0200

    added comments and added flashlight distance stuff

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -26,35 +26,63 @@ func _process(delta):
 	#behavior that doesn't involve player detection
 	match state:
 		"IDLE":
+			#Point in the driection that the enemy is moving
 			angle = rad2deg(Vector2().angle_to_point(go))
 		"CURIOUS","FOUND":
+			#If it is at the target, swing flashlight around in search of the player
+			#If not, point towards target
 			if is_at_target():
+				#TODO add swinging animation with curves
 				angle += 1
 			else:
 				angle = rad2deg(get_position().angle_to_point(target))
 	#behavior that *does* involve player detection
 	if canSeePlayer():
+		var ppos = player.get_position()
+		var distance = get_position().distance_to(ppos)
 		match state:
 			"IDLE":
-				#TODO: Reimplement flashlight distance
-				state = "CURIOUS"
+				#If the guard sees the player, go into curious mode
+				#If the player is right in front of the guard, then the player is found immediately
+				
+				#TODO: add "huh?" past 2/3
+				if distance/sightDistance > (1.0/3.0):
+					state = "CURIOUS"
+				else:
+					state = "FOUND"
 			"CURIOUS":
-				if searchTimer <= CURIOUS_TIMER_MAX:
+				#If sees player, move toward player and count up timer
+				#If timer at max, then the guard has found the player.
+				#Alert level affects time window player has to avoid detection- higher alert level means guards are more wary
+				if searchTimer <= CURIOUS_TIMER_MAX - alert:
 					searchTimer += delta
 				else:
 					state = "FOUND"
-				target = player.get_position()
+				target = ppos
 			"FOUND":
+				#If sees player and found, reset timer used for losing the player
+				#And also track player
+				
+				#TODO: Communicate with other guards
 				seenTimer = SEARCHING_TIMER
-				target = player.get_position()
-	else:
+				target = ppos
+	else: #If does not see player
 		match state:
 			"CURIOUS":
+				#If does not see player, count down the timer that was counting up before
+				#If timer was at 0, then return back to idle
+				
+				#TODO: Set alert level based on how close the player was to discovery?
 				if searchTimer > 0:
 					searchTimer -= delta
 				else:
 					state = "IDLE"
 			"FOUND":
+				#If does not see player, count down timer
+				#If timer at 0, then revert to curious state, set alert level
+				
+				alert+=1
+				
 				if seenTimer > 0:
 					seenTimer -= delta
 				else:
