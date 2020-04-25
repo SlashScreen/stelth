10afa85eab0de8f018c4d5b714b8e86874d344d0
Greyson Jennings <tallesttower1@gmail.com>
2020-04-25 14:44:46 +0200

    ok it does work, linearly. I need it to be non-linear

--- Menu/menucam.gd
+++ Menu/menucam.gd
@@ -6,28 +6,34 @@ var start_angle = 0
 var next_pos : Vector2
 var start_pos : Vector2
 var iterator = 0
+var difference
+var speed = 2
 var moving = false
 
 func go_to(pos,angle): #Angle in Degrees
 	set_position(pos)
-	set_rotation(angle)
-	#next_angle = angle #set target angle
+	#set_rotation(angle)
+	start_angle = get_rotation()
+	next_angle = angle #set target angle
+	difference = next_angle-start_angle
 	#next_pos = pos
-#	moving = true
+	moving = true
 
-#func _process(delta):
-	#TODO: separate rotation and movement
-	#if moving:
-		#iterator+=1
-		#var difference = next_angle-start_angle
-		#var progress
-		#if difference == 0:
-		#	start_angle = next_angle
-		#	start_pos = next_pos
-		#	progress = 1
-		#	moving = false
-		#else:
-		#	progress = iterator / difference
+func _process(delta):
+	#TODO: Rotation is linear, needs to be smooth
+	if moving:
+		iterator+=delta*speed
+		var progress
+		if difference == 0:
+			start_angle = next_angle
+			start_pos = next_pos
+			progress = 1
+			moving = false
+		else:
+			progress = iterator / difference
 		
-		#set_offset(start_pos+(start_pos.direction_to(next_pos).normalized()*swingCurve.interpolate(progress)))
-		#set_rotation(start_angle-(difference*swingCurve.interpolate(progress)))
+		if progress >= 1:
+			moving = false
+			pass
+		
+		set_rotation(start_angle+(difference*progress))
